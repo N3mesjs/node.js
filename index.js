f62efe46baadea84012cb34e7e05d1d5a2fc9ee9
N3mesjs <84539974+N3mesjs@users.noreply.github.com>
2024-11-25 17:13:31 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,3 @@
+// const data = require("./chico")  questo é common js, ma non lo usero infatti nel package specifico che uso module cioe ES6
+                                    
+// console.log(data)
---
+++ server.js
@@ -0,0 +1,45 @@
+import http from 'http'; //funzione integrata di node.js per gestire le REST API e quindi richieste dei client
+const port = 8000;
+
+const server = http.createServer((req, res) => {
+    //res.write("ciao");
+    //res.setHeader('Content-Type', 'text-html'); //setHeader é un metodo che mi permette di modificare la tipologia di dati che viene inviata dal server, in questo caso codice html
+    //res.statusCode = 404; //modifico lo stato della richiesta
+
+    // res.writeHead(201, { //write head racchiude statusCode e setheader
+    //     "Content-Type": "application/json", //ci sono anche molte altre proprieta legate all'header
+    // })
+    // res.end(JSON.stringify({message: "Chico"})); //posso usare direttamente .end per scrivere a schermo
+
+    //console.log(req.url); //qui ce una falla che express.js gestisce in automatico, se vado all'indirizzo http://localhost:3000/about il codice verra eseguito lo stesso, cosa che pero con express non accade dato che viene eseguito del codice quando viene eseguito un metodo specifico(post, get) a uno specifico indirizzo e non a tutti
+    //console.log(req.method);
+
+    // Proviamo a fare come express.js quindi per ogni indirizzo ci sara una funzione diversa
+
+    if(req.url === "/" && req.method === "POST"){
+        res.writeHead(201, {
+            "Content-Type": "application/json",
+        })
+        res.end(JSON.stringify({message: "POST REQUEST"}));
+    } else if(req.url === "/" && req.method === "GET"){
+        res.writeHead(201, {
+            "Content-Type": "text-html",
+        })
+        res.end("<h1>GET REQUEST</h1>");
+    } else {
+        res.writeHead(404, {
+            "Content-Type": "text-html",
+        });
+        res.end("<h1>Page Not Fount <b>404</b></h1>")
+    }
+})
+
+
+server.listen(port, () => { //il server non appena entra in ascolto della porta 3000 esegue la funzione
+    console.log(`Server running on port: ${port}`)
+})
+
+
+
+// NB: ho scaricato nodemon ma ho usato npm i -D nodemon, il -D mi permette di salvarlo come una devDependency quindi non necessaria al funzionamento del programma e facoltativo
+// Nel package.json negli scripts ci sono 2 script standard: test e start, questi due per essere runnati basta scrivere npm start/test invece per script con nome diverso dovro scrivere npm run "nome"
