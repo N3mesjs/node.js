2d1ed2b15b5cc6b5858a64f836d37b6c6898c9b4
N3mesjs <alessiodelsorbo390@gmail.com>
2025-12-18 01:35:58 +0100

    .

--- streams.js
+++ streams.js
@@ -1,4 +1,4 @@
-import { readFileSync, ReadStream, writeFile, writeFileSync } from 'node:fs';
+import { readFileSync, createReadStream, writeFileSync } from 'node:fs';
 
 /**
  * File creato per smanettare un po con il modulo fs e le streams in Node.js
@@ -15,9 +15,22 @@ const fileContent = readFileSync('./bigAss.txt', 'utf-8');
 writeFileSync('./copyOfBigAss.txt', fileContent);
 
 
-const data = ReadStream('./bigAss.txt', { highWaterMark: 16 });
-data.on('data', (chunk) => {
-    console.log(`Ho letto questo pezzo di file: ${chunk}`);
-    writeFileSync('./output.txt', chunk, {flag: 'a'}); //append
+/**
+ * createReadStream restituisce un oggetto del tipo readStream
+ * Presenta diversi event Emitters come 'data', 'end', 'error', ecc.
+ * Qui sotto ascoltiamo l'evento 'data' che viene emesso ogni volta che un chunk di dati é pronto per essere processato.
+ * Il secondo argomento highWaterMark specifica la dimensione del chunk in byte (default 64kb)
+ * l'event data e utile per manipolare i dati, ma se non ho bisogno di manipolarli posso usare anche il 
+ * metodo pipe() per collegare direttamente la readStream ad una writeStream.
+ */
+
+const data = createReadStream('./bigAss.txt', { highWaterMark: 16 });
+data.on('open', () => {
+    console.log('Stream aperto con successo');
 });
+data.on('data', (chunk) => {
+    console.log(chunk);
+});
+
+
 
