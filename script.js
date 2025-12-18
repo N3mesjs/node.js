fe38687eeb6e8547bfeb2279c2683922b1d77bef
N3mesjs <alessiodelsorbo390@gmail.com>
2025-12-19 00:16:28 +0100

    .

--- script.js
+++ script.js
@@ -1,10 +1,42 @@
-async function fetchStream() {
-    const response = await fetch('http://localhost:3010/stream');
-    const result = response.body.getReader();
-    return result;
+/**
+ * Fetches a stream from the given URL and processes it.
+ * This function uses the Fetch API to get a readable stream
+ * we get the stream reader and use the .getReader() method to read the stream.
+ * It is a ReadableStreamDefaultReader object that allows us to read the stream chunk by chunk.
+ * It exposes a .read() method that returns a promise and if it resolves, it returns an object with two properties:
+ * - done: a boolean indicating if the stream is finished
+ * - value: a Uint8Array containing the chunk of data read from the stream
+ * The function reads the stream until done is true.
+ * 
+ * @param {url} string
+ * @returns {Promise<void>} 
+ */
+
+const utf8Decoder = new TextDecoder("utf-8");
+
+async function fetchStream(url){
+    let charsReceived = 0;
+    const response = await fetch(url);
+    const reader = response.body.getReader();
+
+    reader.read().then(function processText({ done, value }){
+        /**
+         * The result is an object with two properties:
+         * - done: a boolean indicating if the stream is finished
+         * - value: a Uint8Array containing the chunk of data read from the stream
+         */
+        if (done) {
+            console.log('Stream completo');
+            return;
+        }
+
+        charsReceived += value.length;
+        const chunk = value;
+        console.log(`Received ${charsReceived} characters so far. Current chunk = ${utf8Decoder.decode(chunk, {stream: true})}`);
+        //return reader.read().then(processText);
+    });
 }
 
-const result = fetchStream();
-console.log(result);
+fetchStream('http://localhost:3010/stream');
 
 document.getElementById('output').innerHTML = result;
--- streamHttp.js
+++ streamHttp.js
@@ -1,3 +1,16 @@
+/**
+ * streamHttp.js
+ * 
+ * This code creates an Express server that streams the contents of 'output.txt' file
+ * to clients making a GET request to the '/stream' endpoint.
+ * It uses the 'fs' module to create a readable stream from the file
+ * and pipes that stream directly to the HTTP response.
+ * N.B. if you don't have 'output.txt' file in the same directory as this script,
+ * it is needed to create the stream manually creating a new ReadableStream object and use the start()
+ * method to push data into the stream. https://developer.mozilla.org/en-US/docs/Web/API/ReadableStream
+ * for reference.
+ */
+
 import express from 'express';
 import { createReadStream } from 'node:fs';
 import cors from 'cors';
@@ -6,7 +19,7 @@ const app = express();
 app.use(cors());
 
 app.get('/stream', (req, res) => {
-    const readStr = createReadStream('./bigAss.txt');
+    const readStr = createReadStream('./output.txt');
     readStr.on('error', (err) => {
         res.status(500).send('Errore nella lettura del file');
         console.log("file non trovato")
